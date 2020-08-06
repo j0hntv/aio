5ee3fd807c7f7837e051f63eb900cabfebc0a58a
J0hn <j0hntvdesign@gmail.com>
2020-08-06 12:38:37 +0300

    Add explosion after hitting a target

--- 02_Through_The_Garbage_To_The_Stars/main.py
+++ 02_Through_The_Garbage_To_The_Stars/main.py
@@ -5,6 +5,7 @@ import random
 import sys
 import time
 from curses_tools import draw_frame, read_controls, get_frame_size
+from explosion import explode
 from obstacles import Obstacle
 from physics import update_speed
 
@@ -87,7 +88,13 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
         while row < rows_number:
             if obstacle in obstacles_in_last_collisions:
                 obstacles_in_last_collisions.remove(obstacle)
+
+                obstacle_center_row = obstacle.row + rows_size // 2
+                obstacle_center_column = obstacle.column + columns_size // 2
+
+                await explode(canvas, obstacle_center_row, obstacle_center_column)
                 return
+
             draw_frame(canvas, row, column, garbage_frame)
             canvas.border()
             await asyncio.sleep(0)
