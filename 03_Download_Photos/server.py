9bf021ec853330a8c6d0610fbd39429d0eb7ca53
J0hn <j0hntvdesign@gmail.com>
2020-08-27 20:43:16 +0300

    Add response streaming

--- 03_Download_Photos/server.py
+++ 03_Download_Photos/server.py
@@ -1,5 +1,31 @@
-from aiohttp import web
+import asyncio
+import datetime
 import aiofiles
+from aiohttp import web
+
+
+INTERVAL_SECS = 1
+
+
+async def uptime_handler(request):
+    
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS)
 
 
 async def archivate(request):
@@ -14,8 +40,9 @@ async def handle_index_page(request):
 
 if __name__ == '__main__':
     app = web.Application()
+    archive_hash = '7kna'
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archivate),
+        web.get(f'/archive/{archive_hash}/', uptime_handler),
     ])
     web.run_app(app)
