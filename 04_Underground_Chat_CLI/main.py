b20aa706978d3529ca49f3cec124559349d12874
J0hn <j0hntvdesign@gmail.com>
2020-10-12 19:11:08 +0300

    Save messages history to file

--- 04_Underground_Chat_CLI/main.py
+++ 04_Underground_Chat_CLI/main.py
@@ -1,4 +1,6 @@
 import asyncio
+import aiofiles
+from datetime import datetime
 
 
 async def tcp_client(host, port):
@@ -7,7 +9,12 @@ async def tcp_client(host, port):
     try:
         while True:
             data = await reader.readline()
+            time = datetime.now().strftime('[%Y.%m.%d %H:%M]')
+            
             print(data.decode().strip())
+            
+            async with aiofiles.open('log.txt', 'a') as file:
+                await file.write(f'{time} {data.decode()}')
 
     except KeyboardInterrupt:
         print('[*] Exit.')
