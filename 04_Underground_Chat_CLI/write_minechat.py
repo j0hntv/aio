7e775e6842d7688fc2290a3fb2151c4c1c36f79a
J0hn <j0hntvdesign@gmail.com>
2020-11-30 16:01:03 +0300

    Refactor code, split by reusable functions

--- 04_Underground_Chat_CLI/write_minechat.py
+++ 04_Underground_Chat_CLI/write_minechat.py
@@ -1,4 +1,5 @@
 import asyncio
+from dotenv import load_dotenv
 import json
 import logging
 
@@ -11,23 +12,21 @@ logger = logging.getLogger('Sender')
 def get_argument_parser():
     parser = configargparse.ArgParser()
     parser.add('--host', env_var='HOST', help='Chat host')
-    parser.add('--port', type=int, env_var='WRITE_PORT', help='Chat port')
-    parser.add('--hash', env_var='ACCOUNT_HASH', help='User account auth hash')
-    parser.add('--message', help='Your message text')
+    parser.add('-p', '--port', type=int, env_var='WRITE_PORT', help='Chat port')
+    parser.add('-t', '--token', env_var='TOKEN', help='User account auth hash')
+    parser.add('-m', '--message', help='Your message text')
     return parser
 
 
-async def register(reader, writer, username):
-    logger.info((await reader.readline()).decode().strip())
-    writer.write('\n'.encode())
-    logger.info((await reader.readline()).decode().strip())
-    writer.write(f'{username}\n'.encode())
+async def register(reader, writer):
+    await read_response(reader)
+    await submit_message(writer, '\n')
 
-    response = await reader.readline()
-    await writer.drain()
+    await read_response(reader)
+    await submit_message(writer, '\n')
 
-    register_info = json.loads(response.decode())
-    logger.info(register_info)
+    response = await read_response(reader)
+    register_info = json.loads(response)
 
     writer.close()
     await writer.wait_closed()
@@ -35,32 +34,48 @@ async def register(reader, writer, username):
     return register_info
 
 
-async def auth(reader, writer, hash):
-    logger.info((await reader.readline()).decode().strip())
-    writer.write((hash + '\n').encode())
-    await writer.drain()
-
-    response = await reader.readline()
-    auth_info = json.loads(response.decode())
-    logger.info(auth_info)
-    if not auth_info:
+async def authorise(reader, writer, token):
+    await read_response(reader)
+    await submit_message(writer, f'{token}\n')
+    response = await read_response(reader)
+    authorise_info = json.loads(response)
+    if not authorise_info:
         print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
         exit()
 
-    return auth_info
+    return authorise_info
 
 
-async def send_message(reader, writer, message):
-    writer.write(f'{message}\n\n'.encode())
+async def submit_message(writer, message):
+    writer.write(message.encode())
     await writer.drain()
 
 
-async def tcp_client(host, port):
+async def read_response(reader):
+    response = await reader.readline()
+    decoded_response = response.decode().strip()
+    logger.info(decoded_response)
+    return decoded_response
+
+
+async def tcp_client(host, port, token=None):
     reader, writer = await asyncio.open_connection(host, port)
 
     try:
-        await auth(reader, writer, ACCOUNT_HASH)
-        await send_message(reader, writer, MESSAGE)
+        if token:
+            await authorise(reader, writer, token)
+        else:
+            register_info = await register(reader, writer)
+            username = register_info['nickname']
+            token = register_info['account_hash']
+            print(f'{username=} {token=}')
+            reader, writer = await asyncio.open_connection(host, port)
+            await authorise(reader, writer, token)
+
+        await submit_message(writer, f'{MESSAGE}\n\n')
+
+    except Exception as error:
+        print(error)
 
     finally:
         writer.close()
@@ -69,13 +84,15 @@ async def tcp_client(host, port):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(name)s %(message)s')
+
+    load_dotenv()
     args = get_argument_parser().parse_args()
 
     HOST = args.host
     PORT = args.port
-    ACCOUNT_HASH = args.hash
+    TOKEN = args.token
     MESSAGE = args.message
 
-    print(f'\n{HOST=}, {PORT=}, {ACCOUNT_HASH=}, {MESSAGE=}\n')
+    print(f'\n{HOST=}, {PORT=}, {TOKEN=}, {MESSAGE=}\n')
 
-    asyncio.run(tcp_client(HOST, PORT))
+    asyncio.run(tcp_client(HOST, PORT, TOKEN))
