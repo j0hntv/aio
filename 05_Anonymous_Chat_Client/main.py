5338351b5336ca86fa125bd9f789542190b84dc4
J0hn <j0hntvdesign@gmail.com>
2020-12-19 15:35:54 +0300

    Add message generator

--- 05_Anonymous_Chat_Client/main.py
+++ 05_Anonymous_Chat_Client/main.py
@@ -1,10 +1,24 @@
 import asyncio
 import gui
+import time
 
-loop = asyncio.get_event_loop()
 
-messages_queue = asyncio.Queue()
-sending_queue = asyncio.Queue()
-status_updates_queue = asyncio.Queue()
+async def generate_msgs(queue: asyncio.Queue):
+    while True:
+        queue.put_nowait(time.time())
+        await asyncio.sleep(1)
 
-loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
+
+async def main():
+    messages_queue = asyncio.Queue()
+    sending_queue = asyncio.Queue()
+    status_updates_queue = asyncio.Queue()
+
+    draw_coroutine = gui.draw(messages_queue, sending_queue, status_updates_queue)
+    msg_coroutine = generate_msgs(messages_queue)
+
+    await asyncio.gather(draw_coroutine, msg_coroutine)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
