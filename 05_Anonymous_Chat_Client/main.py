48726e09b8d097a7201317d68325158d994f5755
J0hn <j0hntvdesign@gmail.com>
2020-12-21 17:11:05 +0300

    Add connection settings

--- 05_Anonymous_Chat_Client/main.py
+++ 05_Anonymous_Chat_Client/main.py
@@ -1,6 +1,20 @@
 import asyncio
-import gui
 import time
+
+import configargparse
+from dotenv import load_dotenv
+
+import gui
+
+
+def get_argument_parser():
+    parser = configargparse.ArgParser()
+    parser.add('--host', default='minechat.dvmn.org', env_var='HOST', help='Host')
+    parser.add('-l', '--listen', type=int, default=5000, env_var='LISTEN', help='Listen port')
+    parser.add('-w', '--write', type=int, default=5050, env_var='WRITE', help='Write port')
+    parser.add('-t', '--token', env_var='TOKEN', help='User account auth hash')
+    parser.add('-u', '--username', help='User name')
+    return parser
 
 
 async def generate_msgs(queue: asyncio.Queue):
@@ -10,6 +24,15 @@ async def generate_msgs(queue: asyncio.Queue):
 
 
 async def main():
+    load_dotenv()
+    args = get_argument_parser().parse_args()
+
+    HOST = args.host
+    LISTEN_PORT = args.listen
+    WRITE_PORT = args.write
+    TOKEN = args.token
+    USERNAME = args.username
+
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
