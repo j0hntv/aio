0dadb3323d391cfe8665672dab968829b13abc58
J0hn <j0hntvdesign@gmail.com>
2020-12-31 19:13:25 +0300

    Add logging

--- 05_Anonymous_Chat_Client/main.py
+++ 05_Anonymous_Chat_Client/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import logging
 import os
 from contextlib import asynccontextmanager
 from tkinter import messagebox
@@ -11,8 +12,19 @@ from dotenv import load_dotenv
 import gui
 
 
+watchdog_logger = logging.getLogger('watchdog')
+
+
 class InvalidToken(Exception):
     pass
+
+
+def setup_logger(logger, fmt='[%(created)d] %(message)s'):
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(fmt)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 
 @asynccontextmanager
@@ -55,6 +67,12 @@ def load_history(filepath, queue):
         queue.put_nowait('========***========\n')
 
 
+async def watch_for_connection(queues):
+    while True:
+        event = await queues['watchdog'].get()
+        watchdog_logger.info(event)
+
+
 async def request(writer, message):
     writer.write(message.encode())
     await writer.drain()
@@ -68,6 +86,7 @@ async def read_response(reader):
 
 async def authorise(reader, writer, token, queues):
     await read_response(reader)
+    queues['watchdog'].put_nowait('Connection is alive. Prompt before auth')
     await request(writer, f'{token}\n')
     response = await read_response(reader)
     authorise_info = json.loads(response)
@@ -78,6 +97,7 @@ async def authorise(reader, writer, token, queues):
 
     queues['status_updates'].put_nowait(gui.ReadConnectionStateChanged.ESTABLISHED)
     queues['status_updates'].put_nowait(gui.NicknameReceived(nickname))
+    queues['watchdog'].put_nowait('Connection is alive. Authorization done')
 
     return authorise_info
 
@@ -93,6 +113,7 @@ async def send_msgs(host, port, token, queues):
         while True:
             message = await queues['sending'].get()
             await request(writer, f'{sanitize(message)}\n\n')
+            queues['watchdog'].put_nowait('Connection is alive. Message sent')
 
 
 async def read_msgs(host, port, queues):
@@ -101,6 +122,7 @@ async def read_msgs(host, port, queues):
         while True:
             data = await reader.readline()
             message = data.decode().strip()
+            queues['watchdog'].put_nowait('Connection is alive. New message in chat')
             queues['messages'].put_nowait(message)
             queues['saving'].put_nowait(message)
 
@@ -129,6 +151,7 @@ async def main():
         'sending': asyncio.Queue(),
         'status_updates': asyncio.Queue(),
         'saving': asyncio.Queue(),
+        'watchdog': asyncio.Queue(),
     }
 
     queues['status_updates'].put_nowait(gui.ReadConnectionStateChanged.INITIATED)
@@ -140,16 +163,19 @@ async def main():
     read_msg_coroutine = read_msgs(HOST, LISTEN_PORT, queues)
     send_msg_coroutine = send_msgs(HOST, WRITE_PORT, TOKEN, queues)
     save_messages_coroutine = save_messages(HISTORYPATH, queues['saving'])
+    watch_for_connection_coroutine = watch_for_connection(queues)
 
     coroutines = [
         draw_coroutine,
         read_msg_coroutine,
         send_msg_coroutine,
         save_messages_coroutine,
+        watch_for_connection_coroutine,
     ]
 
     await asyncio.gather(*coroutines, return_exceptions=True)
 
 
 if __name__ == '__main__':
+    setup_logger(watchdog_logger)
     asyncio.run(main())
