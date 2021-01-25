3af13c80849450beeafc07e638d3a659c9d8aa28
J0hn <j0hntvdesign@gmail.com>
2021-01-25 15:29:17 +0300

    Change gather to create_task_group

--- 05_Anonymous_Chat_Client/gui.py
+++ 05_Anonymous_Chat_Client/gui.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-import asyncio
 from tkinter.scrolledtext import ScrolledText
 from enum import Enum
+
+from anyio import create_task_group, sleep
 
 
 class TkAppClosed(Exception):
@@ -44,7 +45,7 @@ async def update_tk(root_frame, interval=1 / 120):
         except tk.TclError:
             # if application has been destroyed/closed
             raise TkAppClosed()
-        await asyncio.sleep(interval)
+        await sleep(interval)
 
 
 async def update_conversation_history(panel, messages_queue):
@@ -126,8 +127,7 @@ async def draw(messages_queue, sending_queue, status_updates_queue):
     conversation_panel = ScrolledText(root_frame, wrap='none')
     conversation_panel.pack(side="top", fill="both", expand=True)
 
-    await asyncio.gather(
-        update_tk(root_frame),
-        update_conversation_history(conversation_panel, messages_queue),
-        update_status_panel(status_labels, status_updates_queue)
-    )
+    async with create_task_group() as task_group:
+        await task_group.spawn(update_tk, root_frame)
+        await task_group.spawn(update_conversation_history, conversation_panel, messages_queue)
+        await task_group.spawn(update_status_panel, status_labels, status_updates_queue)
