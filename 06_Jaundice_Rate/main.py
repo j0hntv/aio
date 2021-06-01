c6389a7daa946ba9b54a7010c9564affc081665f
J0hn <j0hntvdesign@gmail.com>
2021-06-01 09:32:00 +0300

    Start lesson 6 again

--- 06_Jaundice_Rate/main.py
+++ 06_Jaundice_Rate/main.py
@@ -1,119 +1,17 @@
-import asyncio
-import logging
-from enum import Enum
-
 import aiohttp
-import pymorphy2
-from anyio import create_task_group, run
-from async_timeout import timeout
-
-from adapters import SANITIZERS, ArticleNotFound
-from text_tools import split_by_words, calculate_jaundice_rate
-from utils import get_words_list, get_article_title, setup_logger, check_time
-
-
-logger = logging.getLogger(__name__)
-
-
-FETCH_TIMEOUT = 1
-
-TEST_ARTICLES = [
-    'https://example.com',
-    'https://lenta.ru/articles/2021/03/20/led/',
-    'https://inosmi.ru/social/20210319/249367936q.html',
-    'https://inosmi.ru/social/20210319/249367936.html',
-    'https://inosmi.ru/politic/20210319/249371849.html',
-    'https://inosmi.ru/politic/20210319/249368825.html',
-    'https://inosmi.ru/politic/20210319/249369853.html',
-    'https://inosmi.ru/politic/20210319/249370229.html',
-    'https://inosmi.ru/politic/20210319/249372078.html',
-]
-
-
-class ProcessingStatus(Enum):
-    OK = 'OK'
-    FETCH_ERROR = 'FETCH_ERROR'
-    PARSING_ERROR = 'PARSING_ERROR'
-    TIMEOUT = 'TIMEOUT'
+import asyncio
 
 
 async def fetch(session, url):
-    async with timeout(FETCH_TIMEOUT) as cm:
-        async with session.get(url) as response:
-            response.raise_for_status()
-            return await response.text()
-
-
-async def process_article(session, url, morph, charged_words, results):
-    score = None
-    words_count = None
-    try:
-        html = await fetch(session, url)
-
-        title = get_article_title(html)
-
-        sanitizer = SANITIZERS['inosmi_ru']
-        sanitized_text = sanitizer(html, plaintext=True)
-
-        with check_time(logger):
-            words = split_by_words(morph, sanitized_text)
-
-        score = calculate_jaundice_rate(words, charged_words)
-        words_count = len(words)
-        status = ProcessingStatus.OK.value
-
-    except ArticleNotFound:
-        status = ProcessingStatus.PARSING_ERROR.value
-
-    except aiohttp.ClientError:
-        title = f'URL <{url}> not exist'
-        status = ProcessingStatus.FETCH_ERROR.value
-
-    except asyncio.exceptions.TimeoutError:
-        title = url
-        status = ProcessingStatus.TIMEOUT.value
-
-    results.append(
-        {
-            'title': title,
-            'status': status,
-            'score': score,
-            'words_count': words_count,
-        }
-    )
-
-
-def print_process_article_results(results):
-    for result in results:
-        print()
-        print(f'Заголовок: {result["title"]}')
-        print(f'Статус: {result["status"]}')
-        print(f'Рейтинг: {result["score"]}')
-        print(f'Слов в статье: {result["words_count"]}')
-        print('===')
+    async with session.get(url) as response:
+        response.raise_for_status()
+        return await response.text()
 
 
 async def main():
-    setup_logger(logger, debug=True)
-    morph = pymorphy2.MorphAnalyzer()
-    charged_words = get_words_list('negative_words.txt')
-
-    process_article_results = []
-
     async with aiohttp.ClientSession() as session:
-        async with create_task_group() as task_group:
-            for url in TEST_ARTICLES:
-                args = [
-                    session,
-                    url,
-                    morph,
-                    charged_words,
-                    process_article_results
-                ]
-                await task_group.spawn(process_article, *args)
-
-    print_process_article_results(process_article_results)
+        html = await fetch(session, 'http://example.com')
+        print(html)
 
 
-if __name__ == '__main__':
-    run(main)
+asyncio.run(main())
