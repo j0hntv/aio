4fc872e20254198eec3dc885d28f3971b3a2f99b
J0hn <j0hntvdesign@gmail.com>
2021-07-26 21:05:09 +0300

    Find buses inside a window

--- 07_Buses_On_The_Map/server.py
+++ 07_Buses_On_The_Map/server.py
@@ -15,6 +15,12 @@ buses = {}
 logger = logging.getLogger('server')
 
 
+def is_inside(bounds, lat, lng):
+    is_lat_inside = bounds['south_lat'] < lat < bounds['north_lat']
+    is_lng_inside = bounds['west_lng'] < lng < bounds['east_lng']
+    return all((is_lat_inside, is_lng_inside))
+
+
 async def fetch_coordinates(request):
     ws = await request.accept()
     global buses
@@ -41,8 +47,11 @@ async def talk_to_browser(ws):
 @suppress(ConnectionClosed)
 async def listen_browser(ws):
     while True:
-        message = await ws.get_message()
+        message = json.loads(await ws.get_message())
+        bounds = message['data']
+        buses_inside = [bus for bus in buses.values() if is_inside(bounds, bus['lat'], bus['lng'])]
         logger.debug(message)
+        logger.debug(f'{len(buses_inside)} buses inside bounds')
 
 
 async def handle_browser(request):
