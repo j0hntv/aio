c8627ff093e74a5964026a075d165802ab97643d
J0hn <j0hntvdesign@gmail.com>
2021-07-26 20:27:49 +0300

    Fetch window coordinates from browser

--- 07_Buses_On_The_Map/server.py
+++ 07_Buses_On_The_Map/server.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from functools import partial
 
 import trio
@@ -11,6 +12,7 @@ DELAY = 1
 FETCH_SOCKET = ('127.0.0.1', 8080)
 SEND_SOCKET = ('127.0.0.1', 8000)
 buses = {}
+logger = logging.getLogger('server')
 
 
 async def fetch_coordinates(request):
@@ -25,26 +27,43 @@ async def fetch_coordinates(request):
             break
 
 
-async def talk_to_browser(request):
-    ws = await request.accept()
-
+@suppress(ConnectionClosed)
+async def talk_to_browser(ws):
     while True:
-        try:
-            message = {
-                "msgType": "Buses",
-                "buses": list(buses.values())
-            }
-            await ws.send_message(json.dumps(message, ensure_ascii=False))
-            await trio.sleep(DELAY)
-        except ConnectionClosed:
-            break
+        message = {
+            "msgType": "Buses",
+            "buses": list(buses.values())
+        }
+        await ws.send_message(json.dumps(message, ensure_ascii=False))
+        await trio.sleep(DELAY)
+
+
+@suppress(ConnectionClosed)
+async def listen_browser(ws):
+    while True:
+        message = await ws.get_message()
+        logger.debug(message)
+
+
+async def handle_browser(request):
+    ws = await request.accept()
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(talk_to_browser, ws)
+        nursery.start_soon(listen_browser, ws)
+
+
+def setup_logger(logger, level):
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+    logger.setLevel(level)
 
 
 @suppress(KeyboardInterrupt)
 async def main():
+    setup_logger(logger, level=logging.DEBUG)
     async with trio.open_nursery() as nursery:
         nursery.start_soon(partial(serve_websocket, fetch_coordinates, *FETCH_SOCKET, ssl_context=None))
-        nursery.start_soon(partial(serve_websocket, talk_to_browser, *SEND_SOCKET, ssl_context=None))
+        nursery.start_soon(partial(serve_websocket, handle_browser, *SEND_SOCKET, ssl_context=None))
 
 
 if __name__ == '__main__':
